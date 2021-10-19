33042c1f919991a0008e0cda112f798ca294e392
Mohammad Sulaiman <smensulaiman007@gmail.com>
2021-10-19 16:52:05 +0600

    RecyclerView Added

--- app/src/main/java/com/suffixit/stickynote/MainActivity.java
+++ app/src/main/java/com/suffixit/stickynote/MainActivity.java
@@ -2,26 +2,46 @@ package com.suffixit.stickynote;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+
     private NoteViewModel noteViewModel;
     private List<Note> allNotes;
+    private NoteAdapter noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setUpRecyclerView();
 
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, notes -> {
             allNotes = notes;
-            ((TextView) findViewById(R.id.txtNoteTitle)).setText(notes.get(0).getTitle());
+            noteAdapter.setNotes(allNotes);
         });
+    }
+
+    private void setUpRecyclerView() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        noteAdapter = new NoteAdapter(this, new ArrayList<>());
+        recyclerView.setAdapter(noteAdapter);
     }
 }
---
+++ app/src/main/java/com/suffixit/stickynote/NoteAdapter.java
@@ -0,0 +1,66 @@
+package com.suffixit.stickynote;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.textview.MaterialTextView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
+
+    private Context context;
+    private List<Note> notes;
+
+    public NoteAdapter(Context context, List<Note> notes) {
+        this.context = context;
+        this.notes = notes;
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new NoteViewHolder(LayoutInflater.from(context).inflate(R.layout.note_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+        final Note note = notes.get(position);
+        holder.txtPriority.setText(String.valueOf(note.getPriority()));
+        holder.txtNoteTitle.setText(note.getTitle());
+        holder.txtNoteDescription.setText(note.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    public class NoteViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.txtPriority)
+        MaterialTextView txtPriority;
+        @BindView(R.id.txtNoteTitle)
+        MaterialTextView txtNoteTitle;
+        @BindView(R.id.txtNoteDescription)
+        MaterialTextView txtNoteDescription;
+
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
