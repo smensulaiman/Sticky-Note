b6025b648e8ff9c66a146d7cb462968fd53b7dcb
Al-AminJoy <alaminislam3555@gmail.com>
2021-11-13 13:09:48 +0600

    Person Fragment Added

---
+++ app/src/main/java/com/suffixit/stickynote/PersonalFragment.java
@@ -0,0 +1,41 @@
+package com.suffixit.stickynote;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PersonalFragment extends Fragment {
+
+    @BindView(R.id.btnCategory)
+    Button btnAddCategory;
+
+
+    private PersonalViewModel mViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.personal_fragment, container, false);
+        ButterKnife.bind(this,view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(PersonalViewModel.class);
+    }
+
+}
---
+++ app/src/main/java/com/suffixit/stickynote/PersonalViewModel.java
@@ -0,0 +1,7 @@
+package com.suffixit.stickynote;
+
+import androidx.lifecycle.ViewModel;
+
+public class PersonalViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
