3d437577047de2b86fd021dca35167f28972515b
Suffix IT Limited <apps@suffixit.com>
2021-11-16 16:42:36 +0600

    Weather API Added

---
+++ app/src/main/java/com/suffixit/stickynote/model/weather/Current.java
@@ -0,0 +1,178 @@
+package com.suffixit.stickynote.model.weather;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Current {
+
+    @SerializedName("dt")
+    @Expose
+    public int dt;
+    @SerializedName("sunrise")
+    @Expose
+    public int sunrise;
+    @SerializedName("sunset")
+    @Expose
+    public int sunset;
+    @SerializedName("temp")
+    @Expose
+    public double temp;
+    @SerializedName("feels_like")
+    @Expose
+    public double feelsLike;
+    @SerializedName("pressure")
+    @Expose
+    public int pressure;
+    @SerializedName("humidity")
+    @Expose
+    public int humidity;
+    @SerializedName("dew_point")
+    @Expose
+    public double dewPoint;
+    @SerializedName("uvi")
+    @Expose
+    public int uvi;
+    @SerializedName("clouds")
+    @Expose
+    public int clouds;
+    @SerializedName("visibility")
+    @Expose
+    public int visibility;
+    @SerializedName("wind_speed")
+    @Expose
+    public double windSpeed;
+    @SerializedName("wind_deg")
+    @Expose
+    public int windDeg;
+    @SerializedName("wind_gust")
+    @Expose
+    public double windGust;
+    @SerializedName("weather")
+    @Expose
+    public List<Weather> weather = null;
+
+    public Current() {
+    }
+
+    public int getDt() {
+        return dt;
+    }
+
+    public void setDt(int dt) {
+        this.dt = dt;
+    }
+
+    public int getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(int sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public int getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(int sunset) {
+        this.sunset = sunset;
+    }
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+    public double getFeelsLike() {
+        return feelsLike;
+    }
+
+    public void setFeelsLike(double feelsLike) {
+        this.feelsLike = feelsLike;
+    }
+
+    public int getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getDewPoint() {
+        return dewPoint;
+    }
+
+    public void setDewPoint(double dewPoint) {
+        this.dewPoint = dewPoint;
+    }
+
+    public int getUvi() {
+        return uvi;
+    }
+
+    public void setUvi(int uvi) {
+        this.uvi = uvi;
+    }
+
+    public int getClouds() {
+        return clouds;
+    }
+
+    public void setClouds(int clouds) {
+        this.clouds = clouds;
+    }
+
+    public int getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(int visibility) {
+        this.visibility = visibility;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public int getWindDeg() {
+        return windDeg;
+    }
+
+    public void setWindDeg(int windDeg) {
+        this.windDeg = windDeg;
+    }
+
+    public double getWindGust() {
+        return windGust;
+    }
+
+    public void setWindGust(double windGust) {
+        this.windGust = windGust;
+    }
+
+    public List<Weather> getWeather() {
+        return weather;
+    }
+
+    public void setWeather(List<Weather> weather) {
+        this.weather = weather;
+    }
+}
