64cbd43da25580b9c2fde1f5f95f569601aaafe3
Al-AminJoy <alaminislam3555@gmail.com>
2021-11-20 17:04:34 +0600

    District Local Storage Added

--- app/src/main/java/com/suffixit/stickynote/view/personal/EditPersonalInfoFragment.java
+++ app/src/main/java/com/suffixit/stickynote/view/personal/EditPersonalInfoFragment.java
@@ -6,8 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,11 +17,15 @@ import androidx.fragment.app.DialogFragment;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.gson.Gson;
 import com.suffixit.stickynote.R;
+import com.suffixit.stickynote.database.LocalStorage;
 import com.suffixit.stickynote.model.District;
 import com.suffixit.stickynote.utils.Utils;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import butterknife.BindView;
@@ -36,7 +42,7 @@ public class EditPersonalInfoFragment extends DialogFragment {
 
     @OnClick(R.id.btnUpdate)
     public void update(){
-        listener.onFinishDialog(txtName.getText().toString());
+        listener.onFinishDialog(txtName.getText().toString(),selectedDistrict);
     }
 
     @OnClick(R.id.autoCompleteDistrict)
@@ -46,8 +52,11 @@ public class EditPersonalInfoFragment extends DialogFragment {
 
     private String name;
     private EditPersonalInfoListener listener;
+    private List<District> districtList;
+    private String selectedDistrict;
+
     public interface EditPersonalInfoListener {
-        void onFinishDialog(String name);
+        void onFinishDialog(String name,String district);
     }
 
     public void setDialogListener(EditPersonalInfoListener listener) {
@@ -67,6 +76,7 @@ public class EditPersonalInfoFragment extends DialogFragment {
         if (bundle != null) {
             name = bundle.getString("NAME");
         }
+
     }
 
     @Nullable
@@ -76,29 +86,52 @@ public class EditPersonalInfoFragment extends DialogFragment {
 
         View view = inflater.inflate(R.layout.layout_edit_personal_info, container, false);
         ButterKnife.bind(this, view);
+
         return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getDistrict();
+        setView();
+        setDistrictAdapter();
+        txtDistrict.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectedDistrict = new Gson().toJson(districtList.get(position));
+            }
+        });
+    }
+
     private void setDistrictAdapter() {
+        districtList = new ArrayList<>();
         try {
-            txtDistrict.setAdapter(new ArrayAdapter(getContext(),
-                    R.layout.support_simple_spinner_dropdown_item,
-                    Utils.getAllDistrict(getContext()).getDistricts().stream()
-                            .map(District::getName).collect(Collectors.toList())));
+            districtList = Utils.getAllDistrict(getContext()).getDistricts();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        ArrayAdapter adapter = new ArrayAdapter(getContext(),
+                R.layout.support_simple_spinner_dropdown_item,
+                districtList.stream()
+                        .map(District::getName).collect(Collectors.toList()));
+            txtDistrict.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
     }
 
     private void setView() {
         txtName.setText(name);
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        setView();
-        setDistrictAdapter();
+
+
+    private void getDistrict() {
+        String storedDistrict = LocalStorage.getInstance(getContext()).getDistrict();
+        if (storedDistrict != null) {
+           District district = new Gson().fromJson(storedDistrict,District.class);
+           txtDistrict.setText(district.getName());
+       }
     }
 
     @Override
